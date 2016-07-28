9cbcd1de5aafa24748d3c3239a2cf35d5e94dc67
upendrak <upendra_35@yahoo.com>
2016-07-28 16:32:49 -0700

    convert percentage instead of raw counts

--- evolinc-ii/2.0/final_summary.R
+++ evolinc-ii/2.0/final_summary.R
@@ -5,11 +5,14 @@ dataMatrix <- data.matrix(data)
 dataMatrix <- ifelse(dataMatrix > 0, 1, 0)
   
 plot <- apply(dataMatrix, 2, sum)
+plot <- round(((plot/max(plot))*100))
+
 png(filename="barplot_lincRNA.png",width=min(3600,1800+800*ncol(plot)/10),height=1800,res=300)
-barplot(plot, 
-        main = "LincRNA's across species",
-        col = "Blue",
-        ylab = "Percent A.thaliana homologous lincRNA loci identified",
-        ylim = c(0, max(plot)*1.2),
-        las = 2)
+bargraph <- barplot(plot, 
+        			main = "LincRNA's across species",
+        			col = "Blue",
+        			ylab = "Percent A.thaliana homologous lincRNA loci identified",
+        			ylim = c(0, max(plot)*1.15),
+        			las = 2)
+text(bargraph,plot,labels = plot,pos=3,cex=.8)
 dev.off()
