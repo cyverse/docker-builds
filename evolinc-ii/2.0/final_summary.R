4c3f73fd49ffb425fdda2d3b9396533561abf740
Upendra Devisetty <upendra_35@iPlantAdmins-MacBook-Pro.local>
2016-08-04 17:16:32 -0700

    add getopts library

--- evolinc-ii/2.0/final_summary.R
+++ evolinc-ii/2.0/final_summary.R
@@ -1,18 +1,58 @@
-data <- read.table("final_summary.txt", sep = "\t", head = T)
-data$File.Name <- NULL
-  
-dataMatrix <- data.matrix(data)
+#!/usr/bin/Rscript
+
+library(getopt)
+
+args<-commandArgs(TRUE)
+
+options<-matrix(c('spe', 'q', 1,   "character",
+                  'help', 'h', 0,   "logical"),
+                   ncol=4,byrow=TRUE)
+
+ret.opts<-getopt(options,args)
+
+if ( !is.null(ret.opts$help) ) {
+  cat(getopt(options, usage=TRUE));
+  q(status=1);
+}
+
+species <- ret.opts$spe
+
+dataFile <- read.table("final_summary.txt", sep = "\t", header = TRUE)
+
+dataFile$File.Name <- NULL
+
+dataMatrix <- data.matrix(dataFile)
+
 dataMatrix <- ifelse(dataMatrix > 0, 1, 0)
-  
+
+result <- c()
+
+for (g in colnames(dataMatrix)) { 
+  test2 <- strsplit(g, " ")[[1]]
+  t1 <- substring(test2, 1, 1)
+  t2 <- substring(test2, 2:3)[1]
+  t3 <- paste(t1, t2, sep = ".")
+  result = append(result,t3)
+}
+
+colnames(dataMatrix) <- result
+
 plot <- apply(dataMatrix, 2, sum)
+
 plot <- round(((plot/max(plot))*100))
 
-png(filename="barplot_lincRNA.png",width=min(3600,1800+800*ncol(plot)/10),height=1800,res=300)
-bargraph <- barplot(plot, 
-        			main = "LincRNA's across species",
-        			col = "Blue",
-        			ylab = "Percent A.thaliana homologous lincRNA loci identified",
-        			ylim = c(0, max(plot)*1.15),
-        			las = 2)
+png(filename="lincRNA_barplot.png",width=min(3600,1800+800*ncol(plot)/10),height=1800,res=300)
+
+string1 = "Percent "
+string2 = " homologous lincRNA loci identified"
+new = paste0(string1, species, string2) 
+
+bargraph <- barplot(plot, col = "blue",
+                    ylab = new, ylim = c(0, max(plot)*1.15),
+                    font = 3, yaxt = "n"
+                   ,las = 2)
+
+axis(2)
+                    
 text(bargraph,plot,labels = plot,pos=3,cex=.8)
 dev.off()
