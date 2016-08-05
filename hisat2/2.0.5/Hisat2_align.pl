3e2a8a5a1508a0d0317545ba89c24004ee8a8ac9
Upendra Devisetty <upendra_35@iPlantAdmins-MacBook-Pro.local>
2016-08-05 08:54:09 -0700

    Fix Dockerfile and wrapper script for Hisat2

--- hisat2/2.0.5/Hisat2_align.pl
+++ hisat2/2.0.5/Hisat2_align.pl
@@ -4,10 +4,8 @@ use File::Basename;
 use Data::Dumper;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
 
-
-
 my (@file_query, $database_path, $user_database_path, $annotation_path, 
-$user_annotation_path, $file_names, $root_names, @file_query2,$lib_type,$file_type);
+$user_annotation_path, $file_names, $root_names, @file_query2, $lib_type, $file_type);
 
 
 GetOptions( "file_query=s"      => \@file_query,
@@ -35,7 +33,6 @@ if (@file_query < 1) {
     die "No FASTQ files were supplied\n";
 }
 
-
 # Sanity check for input ref. genome
 unless ($database_path || $user_database_path) {
   die "No reference genome was selected" 
@@ -49,63 +46,51 @@ if ($user_database_path) {
   }
   my $name = basename($database_path, qw/.fa .fas .fasta .fna/);
   print STDERR "hisat2-indexing $name\n";
- my $hisat2b = "hisat2-build";
-   system $hisat2b . " $database_path $name";
-   if ($database_path !~ /$name\.fa$/) {
-      my $new_path = $database_path;
-      $new_path =~ s/$name\.\S+$/$name\.fa/;
-      system "cp $database_path $new_path";
-  }
-  $database_path = $name;
-}
-
-
-my $success = undef;
-
-system "mkdir output";
-
-for my $query_file (@file_query) {
+  my $hisat2b = "hisat2-build";
+  system $hisat2b . " $database_path $name";
+  
+  for my $query_file (@file_query) {
     # Grab any flags or options we don't recognize and pass them as plain text
     # Need to filter out options that are handled by the GetOptions call
     my @args_to_reject = qw(-xxxx);
 
-
     my $second_file = shift @file_query2 if @file_query2;
 
     my $HISAT_ARGS = join(" ", @ARGV);
+    
     foreach my $a (@args_to_reject) {
-	if ($HISAT_ARGS =~ /$a/) {
-	    report("Most TopHat arguments are legal for use with this script, but $a is not. Please omit it and submit again");
-	    exit 1;
-	}
+        if ($HISAT_ARGS =~ /$a/) {
+            report("Most TopHat arguments are legal for use with this script, but $a is not. Please omit it and submit again");
+            exit 1;
+        }
     }
     my $app  = "/usr/bin/hisat2";
- 
     my $format = $file_type;
     my $library = $lib_type;
- 
+
     chomp(my $basename = `basename $query_file`);
     $basename =~ s/\.\S+$//;
 
+    if ($format eq "PE") {
+    	my $align_command = "$app $HISAT_ARGS --rna-strandness $library -x $name -1 $query_file -2 $second_file -p 4 | samtools view -bS - > $query_file.bam";
+    	report("Executing: $align_command\n");
+    	system $align_command;
+    	system "samtools sort $query_file.bam $basename.sorted";
+    	system "mv $basename.sorted.bam  output";
+    	system "samtools index output/$basename.sorted.bam";
+    	system "rm -rf *bam";
+        }
+    elsif($format eq "SE"){
+    	my $align_command = "$app $HISAT_ARGS --rna-strandness $library -x $name -U $query_file -p 4 | samtools view -bS - > $query_file.bam";
+    	report("Executing: $align_command\n");
+    	system $align_command;
+    	system "samtools sort $query_file.bam $basename.sorted";
+    	system "mv $basename.sorted.bam  output";
+    	system "samtools index output/$basename.sorted.bam";
+    	system "rm -rf *bam";
+        }
+   }
 
-        if ($format eq "PE") {
-    my $align_command = "$app $HISAT_ARGS --rna-strandness $library -x Sbicolor -1 $query_file -2 $second_file | samtools view -bS - > $query_file.bam";
-    report("Executing: $align_command\n");
-    system $align_command;
-    system "samtools sort $query_file.bam $basename.sorted";
-    system "mv $basename.sorted.bam  output";
-    system "samtools index output/$basename.sorted.bam";
-    system "rm -rf *bam";
-	}
-	elsif($format eq "SE"){
-    my $align_command = "$app $HISAT_ARGS --rna-strandness $library -x Sbicolor -U $query_file -p 4 | samtools view -bS - > $query_file.bam";
-    report("Executing: $align_command\n");
-    system $align_command;
-    system "samtools sort $query_file.bam $basename.sorted";
-    system "mv $basename.sorted.bam  output";
-    system "samtools index output/$basename.sorted.bam";
-    system "rm -rf *bam";	
-	}
 }
 system "rm -rf *.ht2";
 
@@ -113,3 +98,10 @@ sub report {
     print STDERR "$_[0]\n";
 }
 
+if ($database_path !~ /$name\.fa$/) {
+      my $new_path = $database_path;
+      $new_path =~ s/$name\.\S+$/$name\.fa/;
+      system "cp $database_path $new_path";
+  }
+  $database_path = $name;
+}
