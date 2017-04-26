d9c57938f5ad835664f7813c6234088cd29e80be
upendrak <upendra_35@yahoo.com>
2017-04-26 14:43:33 -0700

    modify the wrapper scritp to include the de genome widget

--- sed/4.2.3_clean_fasta/clean_fasta_wrapper.sh
+++ sed/4.2.3_clean_fasta/clean_fasta_wrapper.sh
@@ -2,12 +2,14 @@
 
 usage() {
       echo ""
-      echo "Usage : sh $0 -i <input file> -o <output file>"
+      echo "Usage : sh $0 -i <input file> -d <de file> -o <output file>"
       echo ""
 
 cat <<'EOF'
 
-  -i </path/to/input filee>
+  -i </path/to/input custom file>
+ 
+  -d </path/to/de genome file>
 
   -o </path/to/output file>
 
@@ -15,10 +17,13 @@ EOF
     exit 0
 }
 
-while getopts ":i:ho:" opt; do
+while getopts ":i:d:ho:" opt; do
   case $opt in
     i)
      input+=("$OPTARG")
+      ;;
+    d)
+     de+=($OPTARG)
       ;;
     o)
      output=$OPTARG
@@ -32,16 +37,42 @@ done
 
 mkdir $output
 
-for f in "${input[@]}"; do
-    extension=${f#*.}
-    if [ "$extension" == "fasta" ]; then
-        filename=$(basename "$f" ".fasta")
-        sed 's/.*|/>/g' $f > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
-    elif [ "$extension" == "fa" ]; then
-        filename=$(basename "$f" ".fa")
-        sed 's/.*|/>/g' $f > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
-   else
-	echo The extension - "$extension" for the file "$f" needs to be either fasta or fa
-   fi
+if [ ! -z $input ]; then
+	for f in "${input[@]}"; do
+    	    extension=${f#*.}
+    	    if [ "$extension" == "fasta" ]; then
+        	filename=$(basename "$f" ".fasta")
+        	sed 's/.*|/>/g' $f > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
+            elif [ "$extension" == "fa" ]; then
+        	filename=$(basename "$f" ".fa")
+           	sed 's/.*|/>/g' $f > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
+    	    elif [ "$extension" == "fas" ]; then
+        	filename=$(basename "$f" ".fas")
+        	sed 's/.*|/>/g' $f > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
+    	    else
+		echo The extension - "$extension" for the file "$f" needs to be either fasta or fas or fa 1>&2
+		rm -r $output
+		exit 64
+   	    fi
+	done
+elif [ ! -z $de ]; then
+        cp $de .
+        file=$(basename "$de")
+	extension=${file#*.}
+        if [ "$extension" == "fasta" ]; then
+           filename=$(basename "$file" ".fasta")
+           sed 's/.*|/>/g' "$file" > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
+        elif [ "$extension" == "fa" ]; then
+           filename=$(basename "$file" ".fa")
+           sed 's/.*|/>/g' "$file" > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
+        elif [ "$extension" == "fas" ]; then
+           filename=$(basename "$file" ".fas")
+           sed 's/.*|/>/g' "$file" > out.txt && mv out.txt $output/"$filename".cleaned."$extension"
+        else
+           echo The extension - "$extension" for the file "$f" needs to be either fasta or fas or fa 1>&2
+           rm -r $output
+           exit 64
+        fi
+	rm "$file"  
+fi
 
-done
